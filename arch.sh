94360d354eec79e9efa1e5642e4350b4e7ed7762
Zhu Ziyan <ziyan.zhu356@gmail.com>
2024-11-16 16:49:36 +0800

    V0.5.1 Testing

--- arch.sh
+++ arch.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 
-echo "Listing available disks:"
+# List available disks
 lsblk
 
-# Assuming the target disk is /dev/sda
-TARGET_DISK="/dev/vda"
+# Ask for target disk
+read -p "Enter the target disk (e.g., /dev/sda): " TARGET_DISK
 
-# Create a new GPT partition table
+# Create GPT partition table
 parted $TARGET_DISK mklabel gpt
 
-# Create the EFI partition (1GB)
+# Create EFI partition (1GB)
 parted $TARGET_DISK mkpart primary fat32 1MiB 1GiB
 parted $TARGET_DISK set 1 boot on
 
-# Create the swap partition (4GB)
+# Create swap partition (4GB)
 parted $TARGET_DISK mkpart primary linux-swap 1GiB 5GiB
 
-# Create the root partition (remaining space)
+# Create root partition (remaining space)
 parted $TARGET_DISK mkpart primary ext4 5GiB 100%
 
-# Format the partitions
+# Format partitions
 mkfs.fat -F32 ${TARGET_DISK}1  # EFI partition
-mkfs.ext4 ${TARGET_DISK}3  # Root partition
 mkswap ${TARGET_DISK}2  # Swap partition
+mkfs.ext4 ${TARGET_DISK}3  # Root partition
 
 # Enable swap
 swapon ${TARGET_DISK}2
 
-# Mount the root partition
+# Mount root partition
 mount ${TARGET_DISK}3 /mnt
 
-# Mount the EFI partition
-mkdir /mnt/boot/efi
+# Mount EFI partition
+mkdir -p /mnt/boot/efi
 mount ${TARGET_DISK}1 /mnt/boot/efi
 
-# Install base system
+# Install base system, GRUB, and required packages
 pacstrap /mnt base base-devel linux linux-firmware grub efibootmgr
 
-# Generate the fstab file
+# Generate fstab file
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Chroot into the new system
@@ -47,7 +47,7 @@ arch-chroot /mnt <<EOF
 ln -sf /usr/share/zoneinfo/Region/City /etc/localtime
 hwclock --systohc
 
-# Localization
+# Set locale
 sed -i '/en_US.UTF-8/s/^#//g' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
@@ -61,21 +61,24 @@ echo "::1         localhost" >> /etc/hosts
 echo "127.0.1.1   myarch.localdomain myarch" >> /etc/hosts
 
 # Install GRUB (UEFI)
-grub-install --target=x86_64-efi --efi-directory=/boot/efi/ --bootloader-id=GRUB --recheck
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
+
+# Create user
+read -p "Enter username for new user: " USERNAME
+useradd -m -G wheel $USERNAME
+passwd $USERNAME
 
 # Set root password
 echo "Set the root password"
 passwd
 
+# Enable sudo for the user (require "wheel" group)
+echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+
 EOF
 
-# Exit chroot
-exit
-
-# Unmount all partitions
+# Unmount and reboot
 umount -R /mnt
-
-# Reboot the system
 reboot
 
